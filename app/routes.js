86668f76a8c4084f63b52c03f12f8023ef1e9ddd
cristiancch <chiorescu.cristian@gmail.com>
2018-06-27 12:17:40 +0300

    Last commit

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@
 // They are all wrapped in the App component, which should contain the navbar etc
 // See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
 // about the code splitting business
-import { getAsyncInjectors } from 'utils/asyncInjectors';
+import {getAsyncInjectors} from 'utils/asyncInjectors';
 
 const errorLoading = (err) => {
   console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
@@ -14,7 +14,7 @@ const loadModule = (cb) => (componentModule) => {
 
 export default function createRoutes(store) {
   // Create reusable async injectors using getAsyncInjectors factory
-  const { injectReducer, injectSagas } = getAsyncInjectors(store); // eslint-disable-line no-unused-vars
+  const {injectReducer, injectSagas} = getAsyncInjectors(store); // eslint-disable-line no-unused-vars
 
   return [
     {
@@ -23,16 +23,66 @@ export default function createRoutes(store) {
       getComponent(nextState, cb) {
         const importModules = Promise.all([
           System.import('containers/HomePage'),
+          System.import('containers/NavigationContainer/reducer'),
+          System.import('containers/NavigationContainer/sagas'),
         ]);
 
         const renderRoute = loadModule(cb);
 
-        importModules.then(([component]) => {
+        importModules.then(([
+                              component,
+                              navigationReducer,
+                              navigationSagas,
+                            ]) => {
+          injectReducer('navigationContainer', navigationReducer.default);
+          injectSagas('navigationContainer', navigationSagas.default);
           renderRoute(component);
         });
-
         importModules.catch(errorLoading);
       },
+      childRoutes: [
+        {
+          path: '/topics/:topicName',
+          name: 'linkListContainer',
+          getComponent(nextState, cb) {
+            const importModules = Promise.all([
+              System.import('containers/LinkListContainer/reducer'),
+              System.import('containers/LinkListContainer/sagas'),
+              System.import('containers/LinkListContainer'),
+            ]);
+
+            const renderRoute = loadModule(cb);
+
+            importModules.then(([reducer, sagas, component]) => {
+              injectReducer('linkListContainer', reducer.default);
+              injectSagas('linkListContainer', sagas.default);
+              renderRoute(component);
+            });
+            importModules.catch(errorLoading);
+          },
+        },
+        {
+          path: '/login',
+          name: 'loginContainer',
+          getComponent(nextState, cb) {
+            const importModules = Promise.all([
+              System.import('containers/LoginContainer/reducer'),
+              System.import('containers/LoginContainer/sagas'),
+              System.import('containers/LoginContainer'),
+            ]);
+
+            const renderRoute = loadModule(cb);
+
+            importModules.then(([reducer, sagas, component]) => {
+              injectReducer('loginContainer', reducer.default);
+              injectSagas('loginContainer', sagas.default);
+              renderRoute(component);
+            });
+
+            importModules.catch(errorLoading);
+          },
+        }
+      ]
     }, {
       path: '*',
       name: 'notfound',
